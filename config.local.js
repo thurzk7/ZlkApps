2d90f51ed74075a77c0b85c2aad55f384b6a3c81
thurzk7 <arthursanune23@gmail.com>
2025-08-12 18:55:40 -0300

    Update config.local.js

--- config.local.js
+++ config.local.js
@@ -1,10 +1,1 @@
-module.exports = {
-  token: "MTQwMTM5Mjk4NDAwNzkwMTMwNA.GcG78k.I8PKCE7w2zQnTScFTRLU2r_EQmCyGfBTdRyMAg",
-  owner: "1218965011527897149",
-  clientid: "1401392984007901304",
-  guild_id: "1230888692005081098",
-  webhook_logs: "https://discord.com/api/webhooks/1404593496077111427/u1ABJYRhgAXRcNwRF2my45LzEdNuWFpRTKQt8mMv0PPRaWoMbVn1BRQMXlnCIankX3DWT",
-  role: "1386891219967410318",
-  secret: "z4LlBCIr1SW5rDGPChpu2viuuMuCv7iF",
-  redirect: "https://zlk-apps.vercel.app"
-};
+
