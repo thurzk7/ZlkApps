75d74dbfabc790197b690310b75c459ec0d73c30
thurzk7 <arthursanune23@gmail.com>
2025-08-13 19:46:34 -0300

    Update index.js

--- databases/index.js
+++ databases/index.js
@@ -1,13 +1,20 @@
-const { JsonDatabase } = require("wio.db");
+const { MongoClient } = require("mongodb");
 
-const dbC = new JsonDatabase({ databasePath: "./databases/dbConfigs.json" });
-const dbP = new JsonDatabase({ databasePath: "./databases/dbPrincipios.json" });
-const users = new JsonDatabase({ databasePath: "./databases/dbUsers.json" });
-const carts = new JsonDatabase({ databasePath: "./databases/dbCarts.json" });
+// URL do MongoDB (você cria uma conta no Atlas e pega a connection string)
+const uri = "mongodb+srv://thurzw_:e3ArHwLV7BaisWpY@cluster0.xhu2n8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
 
-module.exports = {
-    dbC,
-    dbP,
-    users,
-    carts
+let dbC, dbP, users, carts;
+
+async function initDB() {
+    await client.connect();
+    const db = client.db("meuDB"); // nome do seu banco
+
+    // Coleções equivalentes aos JSONs
+    dbC = db.collection("configs");
+    dbP = db.collection("principios");
+    users = db.collection("users");
+    carts = db.collection("carts");
 }
+
+module.exports = { initDB, dbC, dbP, users, carts };
