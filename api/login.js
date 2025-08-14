68c453275f538724032ae22ce2092813293144bb
thurzk7 <arthursanune23@gmail.com>
2025-08-14 17:37:05 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,15 +2,14 @@ const { Router } = require("express");
 const router = Router();
 const discordOauth = require("discord-oauth2");
 const oauth = new discordOauth();
-const { dbP } = require("../databases/index");
+const { getDbP } = require("../databases/index"); // pega função certa
 const { url_apiHost } = require("../config.json");
 
 router.get("/skyoauth2/login", async (req, res) => {
-
-    const clientid = await dbP.get("autoSet.clientid");
-    const secret = await dbP.get("manualSet.secretBot");
-
     try {
+        const clientid = await getDbP("autoSet.clientid");
+        const secret   = await getDbP("manualSet.secretBot");
+
         res.redirect(oauth.generateAuthUrl({
             clientId: clientid,
             clientSecret: secret,
@@ -22,8 +21,7 @@ router.get("/skyoauth2/login", async (req, res) => {
             message: `${err.message}`,
             status: 500
         });
-    };
-
+    }
 });
 
 module.exports = router;
